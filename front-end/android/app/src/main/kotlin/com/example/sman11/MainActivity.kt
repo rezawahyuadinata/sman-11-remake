082a109396943c29dde766d113ab62f0ec38b480
rezawahyuadinata <rezawahyuadinata0301@gmail.com>
2023-03-15 16:47:28 +0700

    first commit

---
+++ front-end/android/app/src/main/kotlin/com/example/sman11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sman11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ front-end/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SmanSebelas());
+
+class SmanSebelas extends StatelessWidget {
+  const SmanSebelas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello semuanya'),
+        ),
+      ),
+    );
+  }
+}
