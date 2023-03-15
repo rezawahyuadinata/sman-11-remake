c183a3e3f97e256eca24d5986b04dbd0e53ffbc9
rezawahyuadinata <rezawahyuadinata0301@gmail.com>
2023-03-15 17:01:11 +0700

    menambahkan backend go

---
+++ back-end/go.mod
@@ -0,0 +1,8 @@
+module github.com/rezawahyuadinata/sman-11-remake
+
+go 1.20
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.7
+)
---
+++ back-end/models/movies.go
@@ -0,0 +1,12 @@
+package models
+
+type Movie struct {
+	MovieID   string `json:"movieid"`
+	MovieName string `json:"moviename"`
+}
+
+type JsonResponse struct {
+	Type    string  `json:"type"`
+	Data    []Movie `json:"data"`
+	Message string  `json:"message"`
+}
